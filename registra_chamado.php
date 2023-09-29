89435f48204f47c345285dc5d3a2436a7f4a47c7
João Nuno <129988575+JoaoNuno96@users.noreply.github.com>
2023-09-29 08:54:05 +0100

    Add files via upload

---
+++ registra_chamado.php
@@ -0,0 +1,25 @@
+<?php
+
+	session_start();
+
+	//ORGANIZAR O TEXTO
+
+	$titulo = str_replace("#", "-", $_POST["titulo"]);
+	$categoria = str_replace("#", "-", $_POST["categoria"]);
+	$descricao = str_replace("#", "-", $_POST["descricao"]);
+
+
+	$texto = $_SESSION["id"] . "#" . $titulo . '#' . $categoria . '#' . $descricao . PHP_EOL;
+
+	//ABRIR FICHEIRO
+	$arquivo = fopen("arquivo.txt", "a");
+
+	//ESCREVER NO FICHEIRO
+	fwrite($arquivo, $texto);
+
+	//FECHAR O FICHEIRO
+	fclose($arquivo);
+
+	header("Location: abrir_chamado.php");
+
+?>
